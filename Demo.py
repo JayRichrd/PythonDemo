06a21148e3adba467781067c6d6b7a83a9a63fc1
cainjiang <cainjiang@tencent.com>
2019-03-14 20:57:24 +0800

    feat(init): 初始化工程

---
+++ Demo.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Time  : 2019-03-14 20:40
+# @Author: cainjiang
+# @File  : Demo.py
+age = 23
+message = "Happy " + str(age) + "rd Birthday"
+print(message)
